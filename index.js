f0287bf23031d21fc7d594e8574c16124f1e4c45
YB L <87692810+dldudqlsdlqslek@users.noreply.github.com>
2025-01-24 17:41:39 +0900

    start

---
+++ index.js
@@ -0,0 +1,24 @@
+async function makeIssue() {
+    const token = process.env.GITHUB_TOKEN;
+    // 요고 1
+    const OWNER = "programers-AIBE1";
+    // 요고 2
+    const REPO = "250124";
+    const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            title: "숫자 뽑기",
+            body: `${Math.floor(Math.random() * 100) + 1}`,
+        })
+    });
+    if (response.ok) {
+        console.log("success");
+    } else {
+        console.log("fail");
+    }
+}
+
+makeIssue();
